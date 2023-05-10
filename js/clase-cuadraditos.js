f246284e09a8db72d6372773eb24e5136d87a78c
Camila Fariña <Camilafarina03@gmail.com>
2023-05-10 19:41:34 -0300

    cuadraditos

--- js/clase-cuadraditos.js
+++ js/clase-cuadraditos.js
@@ -12,6 +12,10 @@ class Cuadrados {
     let cantidadDeFilas = 200;
     let movimiento = frameCount % 20;
 
+    if(mouseX<= 200){ // Condicion de ubicaion del mouse que controla el movimiento
+      movimiento=0;
+      }
+
     for(let i = 0; i<candtidadDeColumnas; i+=1){ //dibuja filas y columnas
       for(let e= 0; e<cantidadDeFilas; e+=2){ 
         let posX= i*20 + movimiento; // movieminto en x 
@@ -19,9 +23,6 @@ class Cuadrados {
         rect(posX, posY, 10, 20);
       }
     }
-    if(mouseX<= 200){ // Condicion de ubicaion del mouse que controla el movimiento
-      movimiento=0;
-      }
 
 
   }
