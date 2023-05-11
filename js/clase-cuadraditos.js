a9c1d19e4ceadae87b5aed78ea058ce72dae625e
GuadalupeHolsman <103451377+GuadalupeHolsman@users.noreply.github.com>
2023-05-10 23:40:53 -0300

    sffdf

--- js/clase-cuadraditos.js
+++ js/clase-cuadraditos.js
@@ -18,12 +18,14 @@ class Cuadrados {
       cantidadDeFilas=100;
       }
 
- 
+    if(mouseX<= 200){ // Condicion de ubicaion del mouse que controla el movimiento
+      
+    }
 
     for(let i = 0; i<candtidadDeColumnas; i+=1){ //dibuja filas y columnas
       for(let e= 0; e<cantidadDeFilas; e+=2){ 
         let posX= i*20 + movimiento; // movieminto en x 
-        let posY= e;
+        let posY= e*20;
         rect(posX, posY, 10, 20);
       }
     }
